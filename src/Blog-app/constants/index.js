8b21a9f9c21bb0edb81d143de6f53c51124d398f
Ritik Rathour <99650054+ritikrathour@users.noreply.github.com>
2024-05-20 00:36:00 +0530

    Update index.js
    remove proxy

--- src/Blog-app/constants/index.js
+++ src/Blog-app/constants/index.js
@@ -1,4 +1,4 @@
-export const BaseURL = "api/v1";
+export const BaseURL = "https://blog-app-backend-v3ca.onrender.com/api/v1";
 export const NavLinks = [
     {link:"Home",id:"001",path:"/"},
     {link:"Blog Posts",id:"002",path:'/blog-posts'},
